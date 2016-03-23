c5b0ba203c5ee11b95f1b2438dde2d0bf26f6dc4
geraldarthur <gerald.a.rich@gmail.com>
2016-03-23 15:14:42 -0700

    copy tweak

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // All test suites will have a name and a list
 exports = module.exports = {
   name: "dataproofer-geo-suite",
+  fullName: "Geographic Data Tests",
   tests: [],      // the list of main tests to be run in the suite
   subtests: [],   // a list of tests that can be triggered by the main tests but wont be run automatically
 }
